8af0d76f8fe354012b325e3b8cfa39876414f908
FutureScript <fernandobaxter1@gmail.com>
2018-09-28 02:41:04 +0000

    automatically backed up by learn

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -13,3 +13,9 @@ describe "#happy_birthday" do
   end
 end
 
+
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
+end
